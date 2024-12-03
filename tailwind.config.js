df7893950d69856b698541aefb0f59f450249e2a
Marr <ammarabdul27@gmail.com>
2024-12-03 21:51:44 +0700

    changed all UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: 'class',
-  content: ['./public/**/*.{html,js}', './index.html', './404.html'],
+  content: ['./public/**/*.{html,js}', './index.html', './blog/index.html', './projects/index.html', './404.html'],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            "l-base": "#eff1f5",
+            "l-on-base": "#4c4f69",
+            "d-base": "#111215",
+            "d-on-base": "rgb(156 163 175)",
+        }
+    },
   },
   plugins: [],
 }
